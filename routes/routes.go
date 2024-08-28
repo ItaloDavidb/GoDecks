21de944737a9515145b694f40ede570eeecd9577
italodavidb <italodavid2@hotmail.com>
2024-08-28 19:41:33 -0300

    feat: better handling to cards, and Delete route

--- routes/routes.go
+++ routes/routes.go
@@ -2,13 +2,14 @@ package routes
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/italodavidb/goCrud/controllers"
+	cards_controllers "github.com/italodavidb/goCrud/controllers"
 )
 
 func LoadRoutes() *mux.Router {
 	router := mux.NewRouter()
-	router.HandleFunc("/api/Cards", controllers.CreateCard).Methods("POST")
-	router.HandleFunc("/api/Cards", controllers.FindAllCards).Methods("GET")
-	router.HandleFunc("/api/Cards/search", controllers.FindCards).Methods("GET")
+	router.HandleFunc("/api/Cards", cards_controllers.CreateCard).Methods("POST")
+	router.HandleFunc("/api/Cards", cards_controllers.FindAllCards).Methods("GET")
+	router.HandleFunc("/api/Cards/search", cards_controllers.FindCards).Methods("GET")
+	router.HandleFunc("/api/Cards", cards_controllers.DeleteCards).Methods("DELETE")
 	return router
 }
